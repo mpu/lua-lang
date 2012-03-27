2f4fc7df6cb9b9303465d0bdcebb8f1c4cc0d37e
Quentin Carbonneaux <qcarbonneaux@gmail.com>
2012-03-27 14:41:06 +0200

    Prefer grouping over softlines.
    The output is much nicer if we use groups instead of softlines, it
    splits at newlines in a cleverer way.

--- Language/Lua/Pretty.hs
+++ Language/Lua/Pretty.hs
@@ -4,6 +4,7 @@ import Language.Lua.Syntax
 import Text.PrettyPrint.Leijen
 
 shift = nest 4
+shiftg = group . shift
 
 instance Pretty (Name) where
     pretty (Name n) = pretty n
@@ -17,20 +18,17 @@ instance Pretty (BinOp) where
     pretty Eq = text "=="
 
 prettyPms :: Pretty a => [a] -> Doc
-prettyPms = encloseSep (char '(') (char ')') (text ", ") . map pretty
-
-prettyParens :: Pretty a => a -> Doc
-prettyParens x = char '(' <> pretty x <> char ')'
+prettyPms = encloseSep lparen rparen (text ", ") . map pretty
 
 instance Pretty (Exp) where
     pretty (EVar n post) = pretty n <> pretty post
     pretty (EFun pms bdy) =
-        shift (text "function" <+> prettyPms pms </> pretty bdy)
-        </> text "end"
-    pretty (EParens e post) = prettyParens e <> pretty post
+        group $ shift (text "function" <+> prettyPms pms <$> pretty bdy)
+            <$> text "end"
+    pretty (EParens e post) = parens (pretty e) <> pretty post
     pretty (ECall fc post) = pretty fc <> pretty post
     pretty (EBinOp o e1 e2) = pretty e1 <+> pretty o <+> pretty e2
-    pretty (EUnOp o e) = pretty o <+> prettyParens e
+    pretty (EUnOp o e) = pretty o <+> parens (pretty e)
     pretty (EAnti s) = empty
     pretty (ENil) = text "nil"
 
@@ -38,37 +36,37 @@ instance Pretty (FunCall) where
     pretty (FC n pms) = pretty n <> prettyPms pms
 
 instance Pretty (PostExp) where
-    pretty (Field n post) = char '.' <> pretty n <> pretty post
-    pretty (Array n post) = char '[' <> pretty n <> char ']' <> pretty post
-    pretty (Access e post) = char '[' <> pretty e <> char ']' <> pretty post
+    pretty (Field n post) = dot <> pretty n <> pretty post
+    pretty (Array n post) = brackets (pretty n) <> pretty post
+    pretty (Access e post) = brackets (pretty e) <> pretty post
     pretty (FCall l post) = prettyPms l <> pretty post
     pretty (Nil) = empty
 
 instance Pretty (Stat) where
     pretty (Do l) =
-        shift (text "do" </> pretty l) </> text "end"
+        group $ shift (text "do" <$> pretty l) <$> text "end"
     pretty (If l e) =
-        text "if" <+> prettyConds l e </> text "end"
+        text "if" <+> prettyConds l e <$> text "end"
       where prettyConds [] Nothing = empty
-            prettyConds [] (Just l) = shift $ text "else" </> pretty l
+            prettyConds [] (Just l) = shiftg $ text "else" </> pretty l
             prettyConds [(e, s)] el =
-                shift (pretty e <+> text "then" </> pretty s) </> prettyConds [] el
+                shiftg (pretty e <+> text "then" <$> pretty s) </> prettyConds [] el
             prettyConds ((e, s) : l) el =
-                shift (pretty e <+> text "then" </> pretty s)
+                shiftg (pretty e <+> text "then" <$> pretty s)
                 </> text "elseif" <+> prettyConds l el
-    pretty (Call fc) = pretty fc <> char ';'
-    pretty (Ret e) = text "return" <+> pretty e <> char ';'
-    pretty (Assign b) = pretty b <> char ';'
+    pretty (Call fc) = pretty fc <> semi
+    pretty (Ret e) = text "return" <+> pretty e <> semi
+    pretty (Assign b) = pretty b <> semi
 
     prettyList [] = empty
     prettyList [s] = pretty s
-    prettyList (s:ss) = pretty s </> pretty ss
+    prettyList (s:ss) = pretty s <$> pretty ss
 
 instance Pretty (Binding) where
     pretty _ = empty
 
     prettyList [] = empty
-    prettyList l = com ln <+> char '=' <+> com le
+    prettyList l = com ln <+> equals <+> com le
         where com :: Pretty a => [a] -> Doc
               com = encloseSep empty empty (text ", ") . map pretty
               ln = map (\(B n _) -> n) l
